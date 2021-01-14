d1bad5158188db99a13f15c44c64820da2a8fe8b
SherlockCat <xfneal@gmail.com>
2021-01-14 16:17:46 +0800

    Update products.mk

--- products.mk
+++ products.mk
@@ -1,10 +1,3 @@
 # 打包清单
 PRODUCT_PACKAGES += \
-    SecurityCenter \
-    MiLinkService2 \
-    InCallUI \
-    FileExplorer \
-    Notes \
-    NotesConfigOverlay \
-    MiuiCompass \
-    SoundRecorder
+    FileExplorer
