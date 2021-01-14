28b86147e1244ca02fc88b793462e2d0a766c37c
SherlockCat <xfneal@gmail.com>
2021-01-14 15:25:58 +0800

    Create products.mk

---
+++ products.mk
@@ -0,0 +1,10 @@
+# 打包清单
+PRODUCT_PACKAGES += \
+    SecurityCenter \
+    MiLinkService2 \
+    InCallUI \
+    FileExplorer \
+    Notes \
+    NotesConfigOverlay \
+    MiuiCompass \
+    SoundRecorder
